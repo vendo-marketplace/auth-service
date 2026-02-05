f1be87d6efde6a01e0d186e328431a26a883db32
HaaaamBurger <gemasclashes@gmail.com>
2026-02-05 19:07:40 +0200

    refactoring

--- src/main/java/com/vendo/auth_service/adapter/out/user/UserClient.java
+++ src/main/java/com/vendo/auth_service/adapter/out/user/UserClient.java
@@ -3,6 +3,7 @@ package com.vendo.auth_service.adapter.out.user;
 import com.vendo.auth_service.domain.user.common.dto.SaveUserRequest;
 import com.vendo.auth_service.domain.user.common.dto.UpdateUserRequest;
 import com.vendo.auth_service.domain.user.common.dto.User;
+import com.vendo.auth_service.domain.user.common.dto.UserExistsResponse;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
@@ -17,7 +18,7 @@ public interface UserClient {
     User getByEmail(@RequestParam String email);
 
     @GetMapping("/exists")
-    boolean existsByEmail(@RequestParam String email);
+    UserExistsResponse existsByEmail(@RequestParam String email);
 
     @PutMapping
     void update(@RequestParam String id, @RequestBody UpdateUserRequest updateUserRequest);
