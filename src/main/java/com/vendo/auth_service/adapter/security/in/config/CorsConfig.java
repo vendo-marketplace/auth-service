0f3460cbc2e08927c19125c226585ab7a4c2072b
HaaaamBurger <gemasclashes@gmail.com>
2026-04-09 21:13:33 +0300

    enabling options request

--- src/main/java/com/vendo/auth_service/adapter/security/in/config/CorsConfig.java
+++ src/main/java/com/vendo/auth_service/adapter/security/in/config/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
+        configuration.setAllowedOriginPatterns(List.of(
                 SERVER_URL,
                 CLIENT_LOCAL_URL,
                 CLIENT_PROD_URL
