2376eaa8781bb2d0a1841056dced3bcea40ef8cf
vlcdck <pridatchenkovladislav@gmail.com>
2026-04-15 23:22:57 +0300

    investigate Zipkin

--- src/main/java/com/vendo/auth_service/ZipkinConfigInvestigator.java
+++ src/main/java/com/vendo/auth_service/ZipkinConfigInvestigator.java
@@ -1,6 +1,8 @@
 package com.vendo.auth_service;
 
+import brave.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,15 @@ public class ZipkinConfigInvestigator implements CommandLineRunner {
     @Value("${management.zipkin.tracing.endpoint:NOT_FOUND}")
     private String zipkinEndpoint;
 
+    @Autowired(required = false)
+    private Tracer tracer;
+
     @Override
     public void run(String... args) throws Exception {
         log.info("=========================================================");
         log.info("ZIPKIN CONFIGURATION");
         log.info("Spring Boot property: {}", zipkinEndpoint);
+        log.info("Micrometer Tracer Bean: {}", tracer != null ? "FOUND (" + tracer.getClass().getSimpleName() + ")" : "NULL (Tracing failed to start!)");
         log.info("=========================================================");
     }
 }
