7f0b1f41ddacab8e0876d4cc46fdf8d188e115a5
Kostiantyn <sinkkostia@gmail.com>
2026-05-23 17:33:14 +0300

    fix deprecated JWT API and mapper warnings

--- src/main/java/com/vendo/auth_service/adapter/user/out/mapper/UserMapper.java
+++ src/main/java/com/vendo/auth_service/adapter/user/out/mapper/UserMapper.java
@@ -4,8 +4,11 @@ import com.vendo.auth_service.application.auth.dto.UserResponse;
 import com.vendo.auth_service.domain.user.model.User;
 import com.vendo.auth_service.infrastructure.config.mapper.MapStructConfig;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(config = MapStructConfig.class)
+@Mapper(config = MapStructConfig.class,
+        componentModel = "spring",
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     UserResponse toResponse(User user);
