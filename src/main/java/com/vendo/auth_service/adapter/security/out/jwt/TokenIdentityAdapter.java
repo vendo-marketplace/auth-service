56c92435dc917eb9a393cdfb430c9b9d1e5f0adc
Kostiantyn <sinkkostia@gmail.com>
2026-06-27 23:01:25 +0300

    kan-62: fixed refresh endpoint

--- src/main/java/com/vendo/auth_service/adapter/security/out/jwt/TokenIdentityAdapter.java
+++ src/main/java/com/vendo/auth_service/adapter/security/out/jwt/TokenIdentityAdapter.java
@@ -1,15 +1,18 @@
 package com.vendo.auth_service.adapter.security.out.jwt;
 
 import com.vendo.auth_service.adapter.security.out.jwt.props.JwtProperties;
+import com.vendo.auth_service.domain.user.exception.UnauthorizedException;
 import com.vendo.auth_service.port.security.TokenIdentityPort;
 import com.vendo.core_lib.utils.StringUtils;
-import com.vendo.security_lib.http.HttpUtils;
 import com.vendo.security_lib.type.TokenClaim;
 import com.vendo.security_starter.jwt.JwtService;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TokenIdentityAdapter implements TokenIdentityPort {
@@ -17,17 +20,21 @@ public class TokenIdentityAdapter implements TokenIdentityPort {
     private final JwtProperties jwtProperties;
 
     @Override
-    public String extractId(String authorization) {
-        String token = HttpUtils.getTokenFrom(authorization);
-        Claims claims = JwtService.extractAll(token, jwtProperties.getSecret().key());
-        return parseId(claims);
+    public String extractId(String token) {
+        try {
+            Claims claims = JwtService.extractAll(token, jwtProperties.getSecret().key());
+            return parseId(claims);
+        } catch (JwtException | IllegalArgumentException e) {
+            log.warn("Token parsing failed: {}", e.getMessage());
+            throw new UnauthorizedException("Invalid or expired refresh token.");
+        }
     }
 
     private String parseId(Claims claims) {
         String id = claims.get(TokenClaim.ID.getClaim(), String.class);
 
         if (StringUtils.isEmpty(id)) {
-            throw new IllegalArgumentException("Id is required.");
+            throw new UnauthorizedException("Invalid or expired refresh token.");
         }
 
         return id;
